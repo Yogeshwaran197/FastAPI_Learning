85a1acc5d1fff0320b04468a9fc354957fde07df
Yogesh <d.yogeshwaran62@gmail.com>
2026-09-12 19:01:05 +0530

    Query params and path params

--- src/routes/productroute.py
+++ src/routes/productroute.py
@@ -1,12 +1,42 @@
+from anyio.itertools import product
+from h11._abnf import status_code
+from fastapi.exceptions import HTTPException
 from fastapi import APIRouter
-from src.utils.utils import get_all_porduct
+from  src.utils.utils import get_all_product
 
 productRoutes = APIRouter()
 
 
 @productRoutes.get("/get_products")
-def getAllProducts():
-    return get_all_porduct()
+def getAllProducts(product_id: int = None):
+    
+    all_product = getAllProducts()
+
+    if not product_id:
+        return all_product
+    
+    for product in all_product:
+        if product['id'] == product_id:
+            return product
+            
+    return HTTPException(
+        status_code = 400,
+        detail = {"erro, id not found"}
+    )
+
+    
+
+@productRoutes.get("/{id}")
+def getOneProduct(id:int):
+    allProducts = get_all_product
+
+    for product in allProducts():
+        if product['id'] == id:
+            return product
+    return HTTPException(
+        status_code = 400,
+        detail = {"erro, id not found"}
+    )
 
 @productRoutes.post("/create_products")
 def creatProducts():
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,7 +2,7 @@ import json
 
 path = r"src\data\products.json"
 
-def get_all_porduct():
+def get_all_product():
     with open(path, 'r') as file:
         return json.load(file)
         
